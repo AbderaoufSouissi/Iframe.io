fb5fa09bdeaa332d7112038a2ac2b29f1985e759
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-11-04 10:16:57 +0100

    frontend: add Footer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div><Header /></div>
+    <div>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
   )
 }
 
