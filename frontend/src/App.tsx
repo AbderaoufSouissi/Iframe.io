2d71f5b9b747c40a9b4dc14a50f2f3f4f5d3abea
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-11-04 11:26:59 +0100

    frontend: toast notfications work as excpected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
+    <div>
+      <Toaster/>
     
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      </div>
   )
 }
 
