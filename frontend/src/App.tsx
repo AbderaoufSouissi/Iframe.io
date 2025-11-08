da215b56eb52cb6c161eac464def066c67104158
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-11-08 11:46:11 +0100

    frontend: test account creation api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import { Toaster } from 'react-hot-toast'
-
+import UserSyncHandler from './components/UserSyncHandler'
 const App = () => {
   return (
     <div>
+      <UserSyncHandler />
       <Toaster/>
     
       <Routes>
